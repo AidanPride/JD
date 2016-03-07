de5c67a3d7cec915f453f30facc87e9ad172c83d
a.aidan@yandex.ru <Aa7030666>
2016-03-07 14:27:36 +0200

    Inner Class

--- Lesson4/Part2/SimpleLinkedList.java
+++ Lesson4/Part2/SimpleLinkedList.java
@@ -7,6 +7,30 @@ public class SimpleLinkedList {
     private Node root;
     private int size;
 
+    public class Node {
+        private Object o;
+        private Node node;
+
+        public Node() {
+        }
+
+        public Node getNode() {
+            return node;
+        }
+
+        public void setNode(Node node) {
+            this.node = node;
+        }
+
+        public Object getO() {
+            return o;
+        }
+
+        public void setO(Object o) {
+            this.o = o;
+        }
+    }
+
     public SimpleLinkedList() {
     }
 
